2f2d60c281ad10cdd9dda37bb38e7369e4637b30
SEOKJIN_HONG <gchsj9838@gmail.com>
2021-04-05 17:09:02 +0900

    리엑트 이벤트

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Say from './app/Say';
 import Counter from './app/Counter';
 import MyComponent from './MyComponent';
 import EventPractice from './app/EventPractice';
+import EventPracticeFun from './app/EventPracticeFun';
 
 const App = () => {
-	return (
-		<div>
-			{/* <Counter></Counter> */}
-			{/* <Say></Say> */}
-			<EventPractice></EventPractice>
-		</div>
-	);
+  return (
+    <div>
+      {/* <Counter></Counter> */}
+      {/* <Say></Say> */}
+      {/* <EventPractice></EventPractice> */}
+      <EventPracticeFun></EventPracticeFun>
+    </div>
+  );
 };
 
 export default App;
