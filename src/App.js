b33aa9714dfaa37a9ea958534f98cbf1c09b1a56
SEOKJIN_HONG <gchsj9838@gmail.com>
2021-04-05 17:59:01 +0900

    ref 사용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import Say from './app/Say';
-import Counter from './app/Counter';
-import MyComponent from './MyComponent';
-import EventPractice from './app/EventPractice';
-import EventPracticeFun from './app/EventPracticeFun';
+// import React from 'react';
+// import Say from './app/Say';
+// import Counter from './app/Counter';
+// import MyComponent from './MyComponent';
+// import EventPractice from './app/EventPractice';
+import React, { Component } from 'react';
+import ValidationSample from './app/validationSample/ValidationSample';
 
-const App = () => {
-  return (
-    <div>
-      {/* <Counter></Counter> */}
-      {/* <Say></Say> */}
-      {/* <EventPractice></EventPractice> */}
-      <EventPracticeFun></EventPracticeFun>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <Counter></Counter> */}
+//       {/* <Say></Say> */}
+//       {/* <EventPractice></EventPractice> */}
+//       {/* <EventPracticeFun></EventPracticeFun> */}
+//       <ValidationSample></ValidationSample>
+//     </div>
+//   );
+// };
+
+// export default App;
