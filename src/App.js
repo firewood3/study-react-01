b8a4af6fb05ec599e17935f0d6260679f84a6eb6
SEOKJIN_HONG <gchsj9838@gmail.com>
2021-04-09 06:20:57 +0900

    컴포넌트의 반복

--- src/App.js
+++ src/App.js
@@ -4,19 +4,13 @@
 // import MyComponent from './MyComponent';
 // import EventPractice from './app/EventPractice';
 import React, { Component } from 'react';
+import IterationSample from './app/IterationSample';
 import ScrollBox from './app/scrollBox/ScrollBox';
 import ValidationSample from './app/validationSample/ValidationSample';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
